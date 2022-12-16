e944eeeaa105529fdb8121072981bda79c244f8e
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2022-12-16 17:23:16 -0300

    primeiro commit app Teste csharp

---
+++ Testando.cs
@@ -0,0 +1,13 @@
+using System;
+using Teste.Testando;
+
+namespace Teste.Testando
+{
+    class ClasseTeste
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste concluído");
+        }
+    }
+}
