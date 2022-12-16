671be2400ab45d402f909758b9d53c9c1b5b3dd2
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2022-12-16 17:37:29 -0300

    Adicionado novas variáveis

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        int idade;
+        int idade1 = 25;
+        var idade2 = 25;
+        var idade3;
+        
         var texto = "Testando";
         Console.WriteLine(texto);
     }
