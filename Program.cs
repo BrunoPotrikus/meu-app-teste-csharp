182eaaa0c340cfef93a78ab9bfd6bd1c44b9cf18
RafaelBortolozoLuxSistemas <rafaelbortolozo@luxsistemas.com.br>
2022-12-16 17:42:47 -0300

    Adicionado constantes

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ class Program
         int idade1 = 25;
         var idade2 = 25;
         var idade3;
+
+        const int IDADE_MINIMA;
+        const int IDADE_MINIMA1 = 25;
+        const var IDADE_MINIMA2 = 25;
+        const var IDADE_MINIMA3;
         
         var texto = "Testando";
         Console.WriteLine(texto);
